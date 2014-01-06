3e688e5f0fe71c1f75145e705cc6a75cff67a1c3
Andreea Olaru <andreea.olaru19@gmail.com>
2014-01-06 02:21:49 +0100

    improved function getTitle for uppercase source code

--- prog.py
+++ prog.py
@@ -6,5 +6,9 @@ def getTitle(url):
 
    start = cont.find('<title>')+7   # Find the position where the title starts
    end = cont.find('</title>',start)   # Find the position where the title ends
-
+   if (start < 7):   # If a starting position was not found
+                  # because the source code is written in capitals
+      start = cont.find('<TITLE>')+7
+      end = cont.find('</TITLE>',start)
+   
    return cont[start:end]
