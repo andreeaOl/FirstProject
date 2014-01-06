801e141394b3b3215312ee2f6d0ac678047cd906
Andreea Olaru <andreea.olaru19@gmail.com>
2014-01-06 02:25:35 +0100

    test added for uppercase

--- prog_test.py
+++ prog_test.py
@@ -1,12 +1,13 @@
 import unittest
 from prog import getTitle
-class TestTex(unittest.TestCase):
+class TestProg(unittest.TestCase):
  
     def setUp(self):
         pass
  
-    def test_url(self):
+    def test_lowercase(self):
         self.assertEqual( getTitle('http://nedbatchelder.com/code/coverage'), 'Ned Batchelder: coverage.py')
-
+    def test_uppercase(self):
+        self.assertEqual(getTitle('http://www.cse.chalmers.se/edu/year/2013/course/TDA452/'), 'Functional Programming 2013, TDA 452, DIT 142')
 if __name__ == '__main__':
     unittest.main()
